a43810b536e0eedcf8ce74a3d967c823bc73f0ab
Nikirovast <44327027+Nikirovast@users.noreply.github.com>
2019-02-26 15:26:45 +0300

    Initial commit

---
+++ server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"Test/handler"
+	"github.com/labstack/echo"
+	"html/template"
+	"io"
+)
+// Define the template registry struct
+type TemplateRegistry struct {
+	templates *template.Template
+}
+
+// Implement e.Renderer interface
+func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	e := echo.New()
+	// Instantiate a template registry and register all html files inside the view folder
+
+	e.Renderer = &TemplateRegistry{
+		templates: template.Must(template.ParseGlob("static/*.html")),
+	}
+	//e.Use(middleware.BasicAuth(Log))
+	e.GET("/ins", handler.Insert)
+	e.GET("/login", handler.Login)
+	e.GET("/show", handler.Show)
+	e.GET("/del", handler.Delete)
+	e.GET("/edit", handler.Edit)
+//	e.GET("/editdone", handler.Editdone)
+	e.Start(":8000")
+}
